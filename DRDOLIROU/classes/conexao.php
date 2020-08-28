3f42623ee9a799889943f98b50851ce1a070acb3
Ner01 <36746073+JhonatanT@users.noreply.github.com>
2020-08-28 17:29:28 -0300

    Create conexao.php

---
+++ DRDOLIROU/classes/conexao.php
@@ -0,0 +1,7 @@
+<?php
+	$hostname="localhost";
+	$username="root";
+	$password="";
+	$dbname="IA_PULMAO";
+	$conn = mysqli_connect( $hostname, $username, $password,$dbname ) or die( ' Erro na conexão ' ); 
+ ?>
